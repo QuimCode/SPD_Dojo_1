89af0056fae6d2aeb77f34b8806ebea59e6cc522
QuimCode <quimafoncode@gmail.com>
2023-05-02 23:25:03 -0300

    V.0.127

--- codigoFuente2.cpp
+++ codigoFuente2.cpp
@@ -10,15 +10,7 @@
 #define LED_AMARILLO_DOS 10
 #define LED_VERDE_UNO 9
 #define LED_VERDE_DOS 8
-#define BUZZER 14
-#define LED_ROJO_TRES 7
-#define LED_ROJO_CUATRO 6
-#define LED_AMARILLO_TRES 5
-#define LED_AMARILLO_CUATRO 4
-#define LED_VERDE_TRES 3
-#define LED_VERDE_CUATRO 2
-#define BOTON 15
-bool extender_tiempo = false;
+#define BUZZER 3
 
 void setup()
 {
@@ -29,37 +21,26 @@ void setup()
   pinMode(LED_VERDE_UNO, OUTPUT);
   pinMode(LED_VERDE_DOS, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-  pinMode(LED_ROJO_TRES, OUTPUT);
-  pinMode(LED_ROJO_CUATRO, OUTPUT);
-  pinMode(LED_AMARILLO_TRES, OUTPUT);
-  pinMode(LED_AMARILLO_CUATRO, OUTPUT);
-  pinMode(LED_VERDE_TRES, OUTPUT);
-  pinMode(LED_VERDE_CUATRO, OUTPUT);
-  pinMode(BOTON, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  extender_luz_verde(digitalRead(BOTON));
-  iniciar_secuencia(LED_VERDE_UNO,LED_VERDE_DOS,LED_ROJO_TRES,LED_ROJO_CUATRO,2000);//led1,led2,led3,led4,delay  
-  extender_luz_verde(digitalRead(BOTON));
-  iniciar_secuencia(LED_AMARILLO_UNO,LED_AMARILLO_DOS,LED_AMARILLO_TRES,LED_AMARILLO_CUATRO,0);
-  extender_luz_verde(digitalRead(BOTON));
-  iniciar_secuencia(LED_ROJO_UNO,LED_ROJO_DOS,LED_VERDE_TRES,LED_VERDE_CUATRO,0);
-  extender_luz_verde(digitalRead(BOTON));
-  iniciar_secuencia(LED_AMARILLO_UNO,LED_AMARILLO_DOS,LED_AMARILLO_TRES,LED_AMARILLO_CUATRO,0);
+  iniciar_secuencia(LED_VERDE_UNO,LED_VERDE_DOS,2000);//led1,led2,delay  
+  iniciar_secuencia(LED_AMARILLO_UNO,LED_AMARILLO_DOS,0);
+  iniciar_secuencia(LED_ROJO_UNO,LED_ROJO_DOS,0);
+  iniciar_secuencia(LED_AMARILLO_UNO,LED_AMARILLO_DOS,0);
 }
 
-void iniciar_secuencia(int led_prender,int led_prender2,int led_prender3,int led_prender4,int delay)
+void iniciar_secuencia(int led_prender,int led_prender2,int demora_prender)
 {
   if(led_prender == LED_ROJO_UNO && led_prender2 == LED_ROJO_DOS)
   {
     Serial.println("Prendiendo leds");
-    prender(led_prender,led_prender2,led_prender3,led_prender4,0);
+    prender(led_prender,led_prender2,0);
     sonar(BUZZER, 500, 200, 4, 500);//pieza,frecuencia,tiempo,vueltas,delay
-    titilar_y_sonar(led_prender,led_prender2,led_prender3,led_prender4,BUZZER,500,200,3,500);//led1,led2,led3,led4,pieza,frecuencia,tiempo,vueltas,delay  
-    apagar(led_prender,led_prender2,led_prender3,led_prender4);
+    titilar_y_sonar(led_prender,led_prender2,BUZZER,500,200,3,500);//led1,led2,pieza,frecuencia,tiempo,vueltas,delay  
+    apagar(led_prender,led_prender2);
     Serial.println("Apagando leds");
   }
   else
@@ -67,50 +48,41 @@ void iniciar_secuencia(int led_prender,int led_prender2,int led_prender3,int led
     if(led_prender == LED_AMARILLO_UNO && led_prender2 == LED_AMARILLO_DOS)
     {
       Serial.println("Prendiendo leds");
-      titilar_y_sonar(led_prender,led_prender2,led_prender3,led_prender4,BUZZER,300,200,3,500); 
+      titilar_y_sonar(led_prender,led_prender2,BUZZER,300,200,3,500); 
       Serial.println("Apagando leds");
     }
     else
     {
       Serial.println("Prendiendo leds");
-      if(extender_tiempo == true)
-      {
-        delay = 7000; 
-        extender_tiempo = false;
-      }
-      prender(led_prender,led_prender2,led_prender3,led_prender4,delay);
-      titilar(led_prender,led_prender2,led_prender3,led_prender4,500);//led1,led2,delay
-      apagar(led_prender,led_prender2,led_prender3,led_prender4);
+      prender(led_prender,led_prender2,demora_prender);
+      titilar(led_prender,led_prender2,500);//led1,led2,delay
+      apagar(led_prender,led_prender2);
       Serial.println("Apagando leds");
     }
   }
 }
 
-void prender(int led_prender,int led_prender2,int led_prender3,int led_prender4,int demora_prender)
+void prender(int led_prender,int led_prender2,int demora_prender)
 {
   digitalWrite(led_prender, HIGH);
   digitalWrite(led_prender2, HIGH);
-  digitalWrite(led_prender3, HIGH);
-  digitalWrite(led_prender4, HIGH);
   delay(demora_prender);
 }
 
-void apagar(int led_apagar,int led_apagar2,int led_apagar3,int led_apagar4)
+void apagar(int led_apagar,int led_apagar2)
 {
   digitalWrite(led_apagar, LOW);
   digitalWrite(led_apagar2, LOW);
-  digitalWrite(led_apagar3, LOW);
-  digitalWrite(led_apagar4, LOW);
 }
 
-void titilar(int led_titilar,int led_titilar2,int led_titilar3,int led_titilar4,int delay_titilar)
+void titilar(int led_titilar,int led_titilar2,int delay_titilar)
 {
   Serial.println("Titilando leds");
   for (int i = 0; i < 3; i++) {        
-    apagar(led_titilar,led_titilar2,led_titilar3,led_titilar4);  
-    delay(delay_titilar);  
-    prender(led_titilar,led_titilar2,led_titilar3,led_titilar4,0);
-    delay(delay_titilar);
+   apagar(led_titilar,led_titilar2);  
+   delay(delay_titilar);  
+   prender(led_titilar,led_titilar2,0);
+   delay(delay_titilar);
   }
 }
 
@@ -118,22 +90,22 @@ void sonar(int piezo,int frecuencia,int tiempo,int cantidad_vueltas,int delay_so
 {  
   Serial.println("Sonando buzzer");
   for (int i = 0; i < cantidad_vueltas; i++) {
-    tone(piezo, frecuencia, tiempo);
-    delay(delay_sonar);
+   tone(piezo, frecuencia, tiempo);
+   delay(delay_sonar);
   }
 }
 
-void titilar_y_sonar(int led_titilar,int led_titilar2,int led_titilar3,int led_titilar4,int piezo,int frecuencia,int tiempo,int cantidad_vueltas,int delay_titilar_sonar)
+void titilar_y_sonar(int led_titilar,int led_titilar2,int piezo,int frecuencia,int tiempo,int cantidad_vueltas,int delay_titilar_sonar)
 {  
   Serial.println("Titilando leds y sonando buzzer");
   if(led_titilar == 11 && led_titilar2 == 10)
   {
     for (int i = 0; i < cantidad_vueltas; i++) {  
    	 tone(piezo, frecuencia, tiempo);
-     prender(led_titilar,led_titilar2,led_titilar3,led_titilar4,0);
+     prender(led_titilar,led_titilar2,0);
      delay(delay_titilar_sonar);
       
-     apagar(led_titilar,led_titilar2,led_titilar3,led_titilar4);
+     apagar(led_titilar,led_titilar2);
      delay(delay_titilar_sonar);
 	}
   }  
@@ -143,22 +115,13 @@ void titilar_y_sonar(int led_titilar,int led_titilar2,int led_titilar3,int led_t
     {
       for (int i = 0; i < cantidad_vueltas; i++) {   
        tone(piezo, frecuencia, tiempo);
-       apagar(led_titilar,led_titilar2,led_titilar3,led_titilar4);
+       apagar(led_titilar,led_titilar2); 
        delay(delay_titilar_sonar); 
         
        tone(piezo, frecuencia, tiempo);
-       prender(led_titilar,led_titilar2,led_titilar3,led_titilar4,0);
+       prender(led_titilar,led_titilar2,0);
        delay(delay_titilar_sonar);
-  	  }   
+	  }   
     }
   } 
-}
-
-void extender_luz_verde(int boton)
-{
-  if(boton == 1)
-  {
-    extender_tiempo = true;
-    Serial.println("La proxima luz verde durara el doble");
-  }
 }
